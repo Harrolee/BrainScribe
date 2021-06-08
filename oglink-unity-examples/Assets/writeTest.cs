2ee928cd9375163f0196af33a1cb952a1ce4c457
Harrolee <Harrolee@users.noreply.github.com>
2021-06-08 10:13:32 -0700

    add remaining ranges to addScanSet()

--- oglink-unity-examples/Assets/writeTest.cs
+++ oglink-unity-examples/Assets/writeTest.cs
@@ -15,12 +15,10 @@ public class writeTest : MonoBehaviour
     public float BrainIncrements = 5; //secs
 
     string filePath = "C:\\temp";
-    StringBuilder line = new StringBuilder();
 
     float incrementCounter = 0;
 
     string baseFileName = "Delta";
-    string header = "AF3, AF4, AF7, AF8, Fp1, Fp2\n";
 
     
     List<EEGFeatureIndex> featureIndexList2;
@@ -42,24 +40,12 @@ public class writeTest : MonoBehaviour
         StartCoroutine(buildCSV(fileTag));
     }
 
-    string buildHeader()
-    {
-        string[] ranges = { "Alpha", "Beta", "Delta", "Gamma" };
-        StringBuilder header = new StringBuilder();
-        for range in ranges {
-            header.AppendFormat(":{0},{1},{2},{3},{4},{5}", );
-        }
-        
-        
-
-        return header;
-    }
-
     IEnumerator buildCSV(string fileTag)
     {
         StringBuilder newLine;
         //build header
         string fileName = baseFileName + fileTag;
+        string header = buildHeader();
         StreamWriter outputFile = writeHeader(filePath, fileName, header);
         
         while (incrementCounter < CSVTime) // create a new row every x seconds with x seconds of data
@@ -83,31 +69,71 @@ public class writeTest : MonoBehaviour
         }
     }
 
+    /* Header:
+     * Every sensor id for every frequency range:
+     *      ie: Alpha:AF3, Alpha:AF4, Alpha:AF7, Alpha:AF8, Alpha:Fp1, Alpha:Fp2, Beta:AF3, Beta:AF4, Beta:AF7, etc
+     */
+    string buildHeader()
+    {
+        string[] ranges = { "Alpha", "Beta", "Delta", "Gamma" };
+        StringBuilder header = new StringBuilder();
+        foreach (string range in ranges)
+        {
+            header.AppendFormat("{0}:AF3, {0}:AF4, {0}:AF7, {0}:AF8, {0}:Fp1, {0}:Fp2", range);
+        }
+        return header.ToString();
+    }
+
     StringBuilder addScanSet()
     {
+        StringBuilder row = new StringBuilder();
         featureIndexList2 = LooxidLinkData.Instance.GetEEGFeatureIndexData(BrainIncrements);
-        for (int i = 0; i < featureIndexList2.Count; i++)
+        // every object in 
+        /*
+        foreach (EEGFeatureIndex featureIndex in featureIndexList2)
         {
 
-            // Delta
-            // one foreach loop like this for all of the ranges
-            // add each of these to a string
-            line.AppendFormat("{0},{1},{2},{3},{4},{5}", featureIndexList2[i].Delta(EEGSensorID.AF3),
-                                featureIndexList2[i].Delta(EEGSensorID.AF4), featureIndexList2[i].Delta(EEGSensorID.AF7),
-                                featureIndexList2[i].Delta(EEGSensorID.AF8), featureIndexList2[i].Delta(EEGSensorID.Fp1),
-                                featureIndexList2[i].Delta(EEGSensorID.Fp2)
-                                );
-            // append this line to the Delta csv
-
+        }*/
+        for (int i = 0; i < featureIndexList2.Count; i++)
+        {
             //Alpha
-            line.AppendFormat("{0},{1},{2},{3},{4},{5}", featureIndexList2[i].Alpha(EEGSensorID.AF3),
+            row.AppendFormat("{0},{1},{2},{3},{4},{5}", featureIndexList2[i].Alpha(EEGSensorID.AF3),
                     featureIndexList2[i].Alpha(EEGSensorID.AF4), featureIndexList2[i].Alpha(EEGSensorID.AF7),
                     featureIndexList2[i].Alpha(EEGSensorID.AF8), featureIndexList2[i].Alpha(EEGSensorID.Fp1),
                     featureIndexList2[i].Alpha(EEGSensorID.Fp2)
                     );
 
+            //Beta
+            row.AppendFormat("{0},{1},{2},{3},{4},{5}", featureIndexList2[i].Beta(EEGSensorID.AF3),
+                    featureIndexList2[i].Beta(EEGSensorID.AF4), featureIndexList2[i].Beta(EEGSensorID.AF7),
+                    featureIndexList2[i].Beta(EEGSensorID.AF8), featureIndexList2[i].Beta(EEGSensorID.Fp1),
+                    featureIndexList2[i].Beta(EEGSensorID.Fp2)
+                    );
+
+            // Delta
+            row.AppendFormat("{0},{1},{2},{3},{4},{5}", featureIndexList2[i].Delta(EEGSensorID.AF3),
+                                featureIndexList2[i].Delta(EEGSensorID.AF4), featureIndexList2[i].Delta(EEGSensorID.AF7),
+                                featureIndexList2[i].Delta(EEGSensorID.AF8), featureIndexList2[i].Delta(EEGSensorID.Fp1),
+                                featureIndexList2[i].Delta(EEGSensorID.Fp2)
+                                );
+            // append this row to the Delta csv
+
+            //Gamma
+            row.AppendFormat("{0},{1},{2},{3},{4},{5}", featureIndexList2[i].Gamma(EEGSensorID.AF3),
+                    featureIndexList2[i].Gamma(EEGSensorID.AF4), featureIndexList2[i].Gamma(EEGSensorID.AF7),
+                    featureIndexList2[i].Gamma(EEGSensorID.AF8), featureIndexList2[i].Gamma(EEGSensorID.Fp1),
+                    featureIndexList2[i].Gamma(EEGSensorID.Fp2)
+                    );
+
+            //Theta
+            row.AppendFormat("{0},{1},{2},{3},{4},{5}", featureIndexList2[i].Theta(EEGSensorID.AF3),
+                    featureIndexList2[i].Theta(EEGSensorID.AF4), featureIndexList2[i].Theta(EEGSensorID.AF7),
+                    featureIndexList2[i].Theta(EEGSensorID.AF8), featureIndexList2[i].Theta(EEGSensorID.Fp1),
+                    featureIndexList2[i].Theta(EEGSensorID.Fp2)
+                    );
+
         }
-        return line;
+        return row;
     }
 
     // create a file, write a header to it, return the StreamWriter object
